f2038cef8c0046239633b58f22e3c56650dd34f6
OracolXor <oracol@oracol.mobi>
2018-07-22 21:05:33 -0400

    line 88 - fix compatibility mode with Android wallet
    changed "oracol" to "oracolxor"

--- src/qt/qrcodedialog.cpp
+++ src/qt/qrcodedialog.cpp
@@ -85,7 +85,7 @@ void QRCodeDialog::genCode()
 
 QString QRCodeDialog::getURI()
 {
-    QString ret = QString("oracol:%1").arg(address);
+    QString ret = QString("oracolxor:%1").arg(address);
     int paramCount = 0;
 
     ui->outUri->clear();
